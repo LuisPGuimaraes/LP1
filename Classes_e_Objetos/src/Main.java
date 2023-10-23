6c6e89b4562c241d9746c1f4576d04b4ea5d8652
Luis Eduardo Plácido Guimarães <127263371+LuisPGuimaraes@users.noreply.github.com>
2023-10-23 16:57:08 -0300

    Instanciando objetos na Main.java

--- Classes_e_Objetos/src/Main.java
+++ Classes_e_Objetos/src/Main.java
@@ -3,8 +3,29 @@ public class Main {
 
 
         Pizza pizza1 = new Pizza("Calabresa", "39.90");
+        
+        Oceano oceano1 = new Oceano ("Pacifico", 660 000 000);
+        Pais pais1 = new Pais (59.11, "Italia")
 
+        
         System.out.println(pizza1.getPreco());
+
+        Animais animais1 = new Animais ("Réptil", "Crocodilo");
+        System.out.println(animais1.getReino(), animais1.getNome());
+        System.out.print(animais1.SetReino(String));
+        System.out.println("Após atualização: ");
+        System.out.println(animais1.getReino(), animais1.getNome);
+
+        Aviao aviao1 = new Aviao ("Embraer", 100);
+        System.out.println(aviao1.getFabricante());
+
+        Moto moto1 = new Moto ("Yamaha", 250.5);
+        System.out.println(pizza1.getPreco());
+
+        
+        System.out.println(pizza1.getPreco());
+        System.out.println(pizza1.getPreco());
+        
 
         }
     }
